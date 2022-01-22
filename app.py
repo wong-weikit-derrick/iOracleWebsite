5d0238189c26eefa4c59130111d348982a85b794
Derrick Wong <darkwing2025@gmail.com>
2022-01-22 11:14:06 +0800

    changed frontend to plotly

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import datetime as dt
 import ta
 from ta.volatility import BollingerBands
-import matplotlib.pyplot as plt
+import plotly.graph_objs as go
 
 st.title('iOracle')
 st.subheader('Predicting the Future Prices of Apple Stock with Machine Learning')
@@ -12,8 +12,6 @@ st.markdown('''
             This Data Science project illustrates how an Ensemble Method with LSTM and Random Forest 
             was used to predict the 5-Day future price of Apple's stock based on the past 30 days worth of data.
             ''')
-
-
 
 # sidebar choices
 side = st.sidebar
@@ -34,15 +32,12 @@ response = requests.get(
     api_url,
     params=params).json()
 
-# compare_df = st.dataframe(response[0])
-
-# pred_df = st.dataframe(response[1])
 compare_df = pd.DataFrame(response[0])
 compare_df.index = pd.to_datetime(compare_df.index)
-compare_df.columns = ['prediction', 'actual']
+
 
 pred_df = pd.DataFrame(response[1])
-# final_df = compare_df.append(pred_df)
+
 
 # Fix index and join compare_df and pred_df
 pred_df.index = [compare_df.index[n]+dt.timedelta(days=7) for n in range(-5, 0)]
@@ -57,24 +52,54 @@ bb_series = pd.DataFrame({'hband':hband, 'mband':mband, 'lband':lband})
 
 final_df = final_df.merge(bb_series, left_index=True, right_index=True)
 
-# Plot actual vs predict graph
-plt.figure(figsize=(14, 5))
-plt.plot(final_df['hband'], '--', color='green')
-plt.plot(final_df['lband'], '--', color='green')
-plt.plot(final_df['actual'], label='actual')
-plt.plot(final_df['prediction'], label='prediction')
-plt.fill_between(final_df.index, final_df['hband'], final_df['lband'], alpha=0.2, color='green')
-plt.legend()
-plt.title('Model Predictions: Actual vs Predicted')
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.grid()
-fig = plt.gcf()
-# Plot error
-# compare_df['error'] = compare_df['prediction'] - compare_df['actual']
-# plt.plot(compare_df['error'], label='error')
-# plt.legend()
-st.pyplot(fig)
+mode1='lines+markers'
+mode2='lines'
 
+fig = go.Figure([
+    
+    go.Scatter(
+        name='Lower Band',
+        x=final_df.index,
+        y=final_df['lband'],
+        mode=mode2,
+        line=dict(color='orange', dash='dash'),
+        showlegend=True
+    ),
+    go.Scatter(
+        name='Upper Band',
+        x=final_df.index,
+        y=final_df['hband'],
+        mode=mode2,
+        line=dict(color='orange', dash='dash'),
+        showlegend=True,
+        fillcolor='rgba(255, 265, 0, 0.2)',
+        fill='tonexty'
+    ),
+        go.Scatter(
+        name='Prediction',
+        x=final_df.index,
+        y=final_df['prediction'],
+        mode=mode1,
+        marker=dict(color='red'),
+        showlegend=True
+    ),
+    go.Scatter(
+        name='Actual',
+        x=final_df.index,
+        y=final_df['actual'],
+        mode=mode1,
+        marker=dict(color='blue'),
+        showlegend=True
+    )
+
+])
+
+fig.update_layout(
+    xaxis_title='Date',
+    yaxis_title='Price',
+    title='Model Predictions: Actual vs Predicted'
+)
+
+st.plotly_chart(fig)
 
 st.metric("MAE in dollars", f"{round(response[2],2)}")
